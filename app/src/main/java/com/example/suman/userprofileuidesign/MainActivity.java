3c539bec44fef7330e9d5f8d56abaff076527fe4
Monster Techno <officialworksuman@gmail.com>
2018-08-09 15:12:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/suman/userprofileuidesign/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.suman.userprofileuidesign;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(1);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
+        setContentView(R.layout.activity_main);
+
+    }
+}
